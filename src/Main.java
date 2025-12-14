6455a51ebd40eaca6a0fe06320ea5357884accd7
Vladimir Predvechnyy <starphoenix21@gmail.com>
2025-12-14 18:35:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Исходные данные
+        int ticketCost = 785; // цена билета
+        int mileCost = 20; // количество рублей за 1 милю
+
+        // Вычисления
+        int quantityMiles = ticketCost / mileCost; // количество миль за покупку билета
+
+       // Вывод результатов
+        System.out.print("За покупку билета на сумму " + (ticketCost) + " руб.");
+        System.out.print(" начислено " + (quantityMiles) + " миль");
+
+    }
+}
